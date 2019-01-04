71238f6a71718aaa86a30b6e7dd4a3c8971b74c9
eric2esca <eric2esca@gmail.com>
2019-01-04 15:36:51 -0600

    changed app.css and setting up heroku

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 let defaultTextColor = "black";
@@ -54,7 +53,7 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Title</h1>
+        <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
         <Aggregate/>
         <Aggregate/>
         <Filter/>
